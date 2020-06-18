5e476ceeb79db311f9e8fa804440242577749b57
Charlie Barth <charliepbarth@gmail.com>
2020-06-18 21:08:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,15 +3,15 @@
 def countdown(number)
   
   while number > 0
+  puts "#{number} SECOND(S)!"
   number -= 1
   
-  puts "#{number} SECOND(S)!"
  
 end
 return "HAPPY NEW YEAR!"
 end
 
-countdown(11)
+countdown(10)
 
 def countdown_with_sleep(number)
   while number > 0
