66e6cd9dc5ad6ffab76cd6bfa52cf184fc4bd910
Charlie Barth <charliepbarth@gmail.com>
2020-06-18 21:04:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,28 @@
-#write your code here
 
-def countdown
+
+def countdown = "number"
+  number = 10
+  while number > 0
+  number -= 1
+  
+  puts "#{number} SECOND(S)!"
+ 
 end
+return "HAPPY NEW YEAR!"
+end
+
+countdown
+
+def countdown_with_sleep
+  number = 5
+  while number > 0
+  number -= 1
+  
+  puts "#{number} SECOND(S)!"
+  sleep 1
+ 
+end
+ return "HAPPY NEW YEAR!"
+end
+
+countdown_with_sleep
